8ea00a03b57b4c835994876c3b011954a3b0e92d
moroz <rook.alex@gmail.com>
2017-08-29 20:47:11 +0300

    some Golovach courses exp

---
+++ src/main/java/Golovach2013_15_2.java
@@ -0,0 +1,39 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+public class Golovach2013_15_2 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        ByteArrayOutputStream data = new ByteArrayOutputStream(120);
+        DataOutputStream dst = new DataOutputStream(data);
+
+        StringBuffer buf = new StringBuffer();
+
+        for (int i = 0; i < 257; i++) {
+            buf.append("a");
+        }
+
+        dst.writeUTF("");
+        dst.writeUTF(buf.toString());
+        dst.flush();
+        printA(data.toByteArray());
+
+        dst.close();
+
+        byte[] A = "A".getBytes("UTF-8");
+        printA(A);
+        byte[] AA = "AAA".getBytes("UTF-8");
+        printA(AA);
+
+    }
+
+    public static void printA(byte[] a) {
+        System.out.println("array data:");
+        System.out.println(Arrays.toString(a));
+    }
+}
