165f7fcc5f508d20fd8a57d19a5d6afc16a9431e
Alex Rook <rook.alex@gmail.com>
2017-09-08 14:19:55 +0300

    start with Section7

---
+++ src/main/java/horstmann/section7/JSection7.java
@@ -0,0 +1,9 @@
+package horstmann.section7;
+
+public class JSection7 {
+
+    @Override
+    public String toString() {
+        return "JSection7 instance";
+    }
+}
---
+++ src/main/scala/horstmann/Section7.scala
@@ -0,0 +1,106 @@
+package horstmann
+
+package level_1 {
+
+  class Level_1_A {
+    override def toString: String = "Level_1_A"
+  }
+
+  class Level_1_B {
+
+    private[level_1] val pp = "private package value"
+
+    override def toString: String = "Level_1_B"
+  }
+
+  package level_2 {
+
+    class Level_2_A {
+      override def toString: String = "Level_2_A"
+    }
+    package level_3 {
+
+      object tst {
+        val level_1_A = new Level_1_A
+        val level_2_A = new Level_2_A
+      }
+
+    }
+
+  }
+
+}
+
+
+package p_l7.p_l74.test {
+
+  class A_74 {
+    override def toString: String = "A_74"
+  }
+
+}
+
+package p_75 {
+
+}
+
+package object p_75 { //object package -> horstmann.p_75
+  val p75_some_value = "p_75 some value"
+}
+
+object Section7 extends App {
+
+  object l7 {
+
+    object l71 {
+
+      import level_1.level_2.Level_2_A
+
+      val a = new Level_2_A
+      println(a)
+    }
+
+    object l72 {
+
+      import section7.JSection7 //relative import
+
+      val jSection7 = new JSection7
+      println(jSection7)
+    }
+
+    object l73 {
+
+      import _root_.horstmann.level_1.level_2.Level_2_A //absolute paths start with _root_
+      val level_2_A = new Level_2_A
+      println(level_2_A)
+
+    }
+
+    object l74 {
+
+      import p_l7.p_l74.test.A_74
+
+      val a_74 = new A_74
+      println(a_74)
+    }
+
+    object l75 {
+      println(p_75.p75_some_value)
+    }
+
+    object l76 {
+
+      import horstmann.level_1.Level_1_B
+
+      val a = new Level_1_B
+      // println(a.pp) //compile error
+    }
+
+  }
+
+  l7.l71
+  l7.l72
+  l7.l73
+  l7.l74
+  l7.l75
+}
