d299e33d918377d1598d7682df3d53c5bba1b8c6
Alex Rook <rook.alex@gmail.com>
2017-09-12 14:47:47 +0300

    Section7 q3,6,7,9

--- src/main/scala/horstmann/Section7.scala
+++ src/main/scala/horstmann/Section7.scala
@@ -1,5 +1,7 @@
 package horstmann
 
+import java.io.{InputStreamReader, LineNumberReader}
+
 package level_1 {
 
   class Level_1_A {
@@ -47,6 +49,29 @@ package p_75 {
 package object p_75 { //object package -> horstmann.p_75
   val p75_some_value = "p_75 some value"
 }
+
+package random {}
+
+package object random {
+
+  private[random] var seed: Double = 0
+  val a = 1664525
+  val b = 1013904223
+  val m = Math.pow(2, 32).toLong
+
+  def nextInt(): Int = nextDouble().toInt
+
+  def nextDouble(): Double = {
+    seed = (seed * a + b) % m
+    seed
+  }
+
+  def setSeed(seed: Int): Unit = {
+    this.seed = seed
+  }
+
+}
+
 
 object Section7 extends App {
 
@@ -98,9 +123,94 @@ object Section7 extends App {
 
   }
 
-  l7.l71
-  l7.l72
-  l7.l73
-  l7.l74
-  l7.l75
+  object q3 {
+
+    import random._
+
+    def testRets() {
+      val count = 5
+      println()
+      var ints: List[Int] = List.empty
+      for (_ <- 0 to count) {
+        ints = nextInt() :: ints
+      }
+      println(ints.sorted)
+      var dbls: List[Double] = List.empty
+      for (_ <- 0 to count) {
+        dbls = nextDouble() :: dbls
+      }
+      println(dbls.sorted)
+    }
+
+    testRets()
+    setSeed(1)
+    testRets()
+  }
+
+  object q6_7 {
+
+    import java.util.{HashSet => JHashSet}
+    import scala.collection.mutable.HashSet
+
+    val jSet = new JHashSet[Int]();
+    for (i <- 0 to 12 by 2) {
+      jSet.add(i)
+    }
+
+    println("java set:" + jSet)
+
+    val set = HashSet.empty[Int]
+    val iter = jSet.iterator()
+
+    while (iter.hasNext) set += iter.next()
+    assert(jSet.size() == set.size)
+    println("scala set:" + set)
+
+
+  }
+
+  object q9 {
+
+    def isSecretEnough(password: String): Boolean =
+      (password.nonEmpty) && (password.length > 8) && (password.matches("[\\w+\\d+]+"))
+
+
+    def run(): Unit = {
+      import java.lang.System._
+
+      val user = getProperty("user.name")
+
+      val reader = new LineNumberReader(new InputStreamReader(in))
+      while (true) {
+        println(s"Enter password for $user>")
+        val pass = reader.readLine()
+        if (pass.equalsIgnoreCase("quit")) return
+        if (isSecretEnough(pass)) {
+          println(s"Welcome $user")
+        } else println("bad password")
+      }
+
+
+    }
+
+    //    println(isSecretEnough(""))
+    //    println(isSecretEnough("123"))
+    //    println(isSecretEnough("123456 789"))
+    //    println(isSecretEnough("123456789"))
+    //    println(isSecretEnough("1234567_sdcsd89"))
+
+    run()
+
+  }
+  scala.StringBuilder
+
+  //  l7.l71
+  //  l7.l72
+  //  l7.l73
+  //  l7.l74
+  //  l7.l75
+
+  q3
+  q6_7
+  q9
 }
