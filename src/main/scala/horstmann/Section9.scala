0e08a43b7df9d132c80e677c123fe029331eac3b
Alex Rook <rook.alex@gmail.com>
2017-09-26 12:05:29 +0300

    Section9 small exp with nio Byte&Char buffers

--- src/main/scala/horstmann/Section9.scala
+++ src/main/scala/horstmann/Section9.scala
@@ -1,4 +1,5 @@
 package horstmann
+
 import java.nio.charset.Charset
 import java.nio.{ByteBuffer, CharBuffer}
 import java.io.File
@@ -11,85 +12,100 @@ import java.nio.file.StandardOpenOption
   */
 object Section9 extends App {
 
-  object TestCharsets1 {
+  object l9 {
 
-    println("--------TestCharsets1------")
+    object TestCharsets1 {
 
-    //    import scala.collection.JavaConverters._
-    //    val c = Charset.availableCharsets().asScala
-    //    for (a <- c) {
-    //      println(a)
-    //    }
+      println("--------TestCharsets1------")
 
-    val charset = Charset.forName("koi8-r")
+      //    import scala.collection.JavaConverters._
+      //    val c = Charset.availableCharsets().asScala
+      //    for (a <- c) {
+      //      println(a)
+      //    }
 
-    println("\n---encoded---")
-    val bb: ByteBuffer = charset.encode("текст на кириллице")
-    val cb = bb.asCharBuffer()
-    while (cb.hasRemaining) {
-      val c = cb.get()
-      print(c) //здесь будут не верные символы поскольку метод bb.asCharBuffer() предполагает наличие в буфере байтов в UTF-16BE
+      val charset = Charset.forName("koi8-r")
+
+      println("\n---encoded---")
+      val bb: ByteBuffer = charset.encode("текст на кириллице")
+      val cb = bb.asCharBuffer()
+      println(cb.getClass.getSimpleName)
+      while (cb.hasRemaining) {
+        val c = cb.get()
+        print(c) //здесь будут не верные символы поскольку метод bb.asCharBuffer() предполагает наличие в буфере байтов в UTF-16BE
+        //see also java.nio.Bits.getCharB + makeChar
+      }
+
+      println("\n---decoded---")
+      val cb2: CharBuffer = charset.decode(bb)
+      while (cb2.hasRemaining) {
+        val c = cb2.get()
+        print(c)
+      }
+      println
     }
 
-    println("\n---decoded---")
-    val cb2 = charset.decode(bb)
-    while (cb2.hasRemaining) {
-      val c = cb2.get()
-      print(c)
+    object TestBuff1 {
+      println("--------TestBuff1------")
+      var charset = Charset.forName("koi8-r")
+      val bb1: ByteBuffer = charset.encode("\n")
+      //bb1.flip() //out flipped after encode!
+
+      //val a: Array[Byte] = new Array[Byte](bb1.limit)
+      val bb2 = ByteBuffer.allocate(bb1.limit)
+
+      println(bb1.hasRemaining)
+
+      while (bb1.hasRemaining) {
+        bb2.put(bb1.get())
+      }
+      println(bb1.remaining) //zero after read
+      println(bb2.remaining) // zero after fill
+      bb1.flip()
+      bb2.flip()
+      println(bb1.remaining)
+      println(bb2.remaining)
+      println(s"buffers equals? :${bb1.equals(bb2)}")
+
+      charset = Charset.forName("utf-8")
+
+      val bb3 = charset.encode("\n")
+      //bb1.flip() //equals do not touches internal state
+      println(bb1.equals(bb3)) //true because '\n' code <127
+
     }
-    println
+
+    object TestBuff2 {
+
+      val buf = ByteBuffer.allocate(10)
+
+      for (x <- 0 until buf.capacity()) {
+        buf.put(1.toByte)
+        print(s"$x\t")
+      }
+      println("\nposition:" + buf.position)
+
+    }
+
+
+    object l91 {
+      val a: Byte = 127;
+      val b: Int = 127;
+      println(a == b) //true
+      val c: Byte = 127
+      println((c & 0xff)) //127 as Int
+      println((c & 0xff).getClass) // int
+    }
+
+      TestCharsets1
+
+//    TestBuff1
+    //    TestBuff2
+    //l91
+
   }
 
-  object TestBuff1 {
-    println("--------TestBuff1------")
-    var charset = Charset.forName("koi8-r")
+  l9
 
-    val bb1 = charset.encode("\n")
-
-    bb1.flip()
-
-    val a: Array[Byte] = new Array[Byte](bb1.limit)
-    val bb2 = ByteBuffer.allocate(bb1.limit)
-
-    while (bb1.hasRemaining) {
-      bb2.put(bb1.get())
-    }
-    println(bb1.equals(bb2))
-
-    charset = Charset.forName("utf-8")
-
-    val bb3 = charset.encode("\n")
-    println(bb1.equals(bb3))
-
-  }
-
-  object TestBuff2 {
-
-    val buf = ByteBuffer.allocate(10)
-
-    for (x <- 0 to buf.capacity() - 1) {
-      print(x + "\t")
-      buf.put(1.toByte)
-
-
-    }
-    println("\nposition:" + buf.position)
-
-  }
-
-  //  TestCharsets1
-  //
-  //  TestBuff1
-  //  TestBuff2
-
-val a:Byte=1;
-
-  val b:Int=1;
-
-  println(a==b)
-
-  val c:Byte= 127
-  println((c & 0xff))
-  println((c & 0xff).getClass)
 
 }
